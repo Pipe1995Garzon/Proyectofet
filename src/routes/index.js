e806ffa28cc5468e9095afd15d7a2fd85dc6daf6
juanpablo1005 <45340428+juanpablo1005@users.noreply.github.com>
2022-03-22 20:35:41 -0500

    subida prueba

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('this is data bank FET');
+    res.send('Hola Amigos');
 })
 
 module.exports = router;
