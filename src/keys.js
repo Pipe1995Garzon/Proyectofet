7c95580af1c394544375abe09db49c84099343b4
Pipe1995Garzon <47527537+Pipe1995Garzon@users.noreply.github.com>
2022-04-01 02:19:06 -0500

    actualizacion keys
    una conexion para que funcione local y la otra para que funcione en el hostikng

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,18 @@
+/*
 module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'banco_datos_fet'
+    }
+}
+*/
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'gestqljc@localhost',
+        password: '0J5Y8SQnpnR3',
+        database: 'gestqljc_gestionfetdb'
     }
 }
